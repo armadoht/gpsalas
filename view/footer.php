d873a9e42e7973b30df4daa179576c9ab8d81434
ahuerta <hutarmando10911@gmail.com>
2019-06-24 18:12:06 -0500

    Modificación del pie de pagina

---
+++ view/footer.php
@@ -0,0 +1,71 @@
+	
+	<footer>
+		<div class="container">
+			<div class="row">
+				<div class="col-md-4">
+					<div class="widget">
+						<h4>Responsables de Salas Corporativo</h4>
+						<p></p>
+						<ul>
+							<li><i class="fa fa-angle-right"></i> <a href="mailto:arosas@grupak.com.mx">Araceli Rosas(Sala de Dirección TIC)</a></li>
+							<li><i class="fa fa-angle-right"></i> <a href="mailto:arosas@grupak.com.mx">Araceli Rosas(Sala TIC)</a></li>
+							<li><i class="fa fa-angle-right"></i> <a href="mailto:arosas@grupak.com.mx">Araceli Rosas(Sala de Dirección de Administración)</a></li>
+							<li><i class="fa fa-angle-right"></i> <a href="mailto:opetlacalco@grupak.com.mx">Oscar Petlacalco(Sala de Capacitación)</a></li>
+						</ul>
+					</div>
+				</div>
+				<!-- .\end col-md-4 -->
+				<div class="col-md-4">
+					<div class="widget">
+						<h4>Responsables de Salas Cuernavaca</h4>
+						<p></p>
+						<ul>
+							<li><i class="fa fa-angle-right"></i> <a href="mailto:tpm.cuernavaca@grupak.com.mx">Irma Figueroa(Sala de Juntas Dirección Papel)</a></li>
+							<li><i class="fa fa-angle-right"></i> <a href="mailto:olopez@grupak.com.mx">Omar Lopez(Sala Jose Gomez)</a></li>
+							<li><i class="fa fa-angle-right"></i> <a href="mailto:ymendoza@grupak.com.mx">Yair Mendoza(Sala Jose Gomez)</a></li>
+							<li><i class="fa fa-angle-right"></i> <a href="mailto:tpm.cuernavaca@grupak.com.mx">Juan Jose Flores(Dominik Tatarka)</a></li>
+							<li><i class="fa fa-angle-right"></i> <a href="mailto:tpm.cuernavaca@grupak.com.mx">Juan Jose Flores(Sala de Tpm)</a></li>
+							<li><i class="fa fa-angle-right"></i> <a href="#">Marco Ocampo(Sala de Juntas Gerencia Papel)</a></li>
+						</ul>
+					</div>
+				</div>
+				<!-- .\end col-md-4 -->
+				<div class="col-md-4">
+						<div class="widget">
+							<h4>Responsables de Salas Toluca</h4>
+							<p></p>
+							<ul>
+								<li><i class="fa fa-angle-right"></i> <a href="mailto:omaldonado@grupak.com.mx">Olga Cristina Maldonado(Sala de Capacitación)</a></li>
+								<li><i class="fa fa-angle-right"></i> <a href="#">Dora Benjume(Sala de Juntas Corrugado)</a></li>
+								<li><i class="fa fa-angle-right"></i> <a href="#">Dora Benjume(Sala de Dirección Corrugado)</a></li>
+							</ul>
+						</div>
+				</div>
+				<!-- .\end col-md-4 -->
+				<div class="col-md-4">
+						<div class="widget">
+							<h4>Responsables de Salas Hidalgo</h4>
+							<p></p>
+							<ul>
+								<li><i class="fa fa-angle-right"></i> <a href="mailto:arivera@grupak.com.mx">Ariana Rivera(Sala de Juntas)</a></li>
+								<li><i class="fa fa-angle-right"></i> <a href="mailto:ysalazar@grupak.com.mx?cc=ddominguez@grupak.com.mx;lrojas@grupak.com.mx">Yessica Salazar(Sala de Capacitación)</a></li>
+								<li><i class="fa fa-angle-right"></i> <a href="mailto:elopez@grupak.com.mx">Edith Lopez (Sala TIC)</a></li>
+							</ul>
+						</div>
+				</div>
+				<!-- .\end col-md-4 -->
+			</div>
+			<!-- .\ row -->
+
+			<div class="row">
+				<hr/>
+				<div class="col-md-12">
+					<div class="copy pull-left">
+						<p> Copyright &copy; <a href="#">www.grupak.com.mx</a> | Designed by <a href="#"></a>Ing. Armando Huerta Tolentino</a></p>
+					</div>
+				</div>
+			</div>
+
+		</div>
+	</footer>
+	
--- view/loginView.php
+++ view/loginView.php
@@ -108,77 +108,7 @@
 
 	<div class="bor"></div>
 	<!-- Start Footer -->
-	<footer>
-		<div class="container">
-			<div class="row">
-				<div class="col-md-4">
-					<div class="widget">
-						<h4>Responsables de Salas Corporativo</h4>
-						<p></p>
-						<ul>
-							<li><i class="fa fa-angle-right"></i> <a href="#">Araceli Rosas(Sala Oeste)</a></li>
-							<li><i class="fa fa-angle-right"></i> <a href="#">Diana Sanchez(Sala Oeste)</a></li>
-							<li><i class="fa fa-angle-right"></i> <a href="#">Luana Ursidio(Sala Oeste)</a></li>
-							<li><i class="fa fa-angle-right"></i> <a href="#">Nancy Bacao(Sala Tic)</a></li>
-							<li><i class="fa fa-angle-right"></i> <a href="#">Oscar Petlacalco(Sala de Capacitación)</a></li>
-						</ul>
-					</div>
-				</div>
-				<!-- .\end col-md-4 -->
-				<div class="col-md-4">
-					<div class="widget">
-						<h4>Responsables de Salas Cuernavaca</h4>
-						<p></p>
-						<ul>
-							<li><i class="fa fa-angle-right"></i> <a href="mailto:ifigueroa@grupak.com.mx">Irma Figueroa(Sala de Juntas Direccion Papel)</a></li>
-							<li><i class="fa fa-angle-right"></i> <a href="mailto:mnavad@grupak.com.mx">Miguel Angel Nava(Sala de Juntas Papel)</a></li>
-							<li><i class="fa fa-angle-right"></i> <a href="mailto:tpm.cuernavaca@grupak.com.mx">Juan Jose Flores(Dominik Tatarka)</a></li>
-							<li><i class="fa fa-angle-right"></i> <a href="mailto:mnava@grupak.com.mx">Myra Nava(Sala de Corrugado)</a></li>
-							<li><i class="fa fa-angle-right"></i> <a href="mailto:olopez@grupak.com.mx">Oscar Omar López(Sala Jose Gomez)</a></li>
-						</ul>
-					</div>
-				</div>
-				<!-- .\end col-md-4 -->
-				<div class="col-md-4">
-						<div class="widget">
-							<h4>Responsables de Salas Toluca</h4>
-							<p></p>
-							<ul>
-								<li><i class="fa fa-angle-right"></i> <a href="#">Benjamin Torres(Sala de Capacitación)</a></li>
-								<li><i class="fa fa-angle-right"></i> <a href="#">Dora Benjume(Sala de Juntas Corrugado)</a></li>
-								<li><i class="fa fa-angle-right"></i> <a href="#">Dora Benjume(Sala de Dirección Corrugado)</a></li>
-								<li><i class="fa fa-angle-right"></i> <a href="#">Daniel Delgado(Sala de Tic)</a></li>
-							</ul>
-						</div>
-				</div>
-				<!-- .\end col-md-4 -->
-				<div class="col-md-4">
-						<div class="widget">
-							<h4>Responsables de Salas Hidalgo</h4>
-							<p></p>
-							<ul>
-								<li><i class="fa fa-angle-right"></i> <a href="mailto:arivera@grupak.com.mx">Ariana Rivera (Sala de Juntas)</a></li>
-								<li><i class="fa fa-angle-right"></i> <a href="mailto:ysalazar@grupak.com.mx?cc=ddominguez@grupak.com.mx;lrojas@grupak.com.mx">Yessica Salazar (Sala de Capacitación)</a></li>
-								<li><i class="fa fa-angle-right"></i> <a href="mailto:ualonso@grupak.com.mx">Ulises Alonso (Sala Tic)</a></li>
-								<li><i class="fa fa-angle-right"></i> <a href="mailto:elopez@grupak.com.mx">Edith Lopez (Sala Webex)</a></li>
-							</ul>
-						</div>
-				</div>
-				<!-- .\end col-md-4 -->
-			</div>
-			<!-- .\ row -->
-
-			<div class="row">
-				<hr/>
-				<div class="col-md-12">
-					<div class="copy pull-left">
-						<p> Copyright &copy; <a href="#">www.grupak.com.mx</a> | Designed by <a href="#"></a>Ing. Armando Huerta Tolentino</a></p>
-					</div>
-				</div>
-			</div>
-
-		</div>
-	</footer>
+	<?php include('footer.php'); ?>
 	<!-- .\ End -->
 
 
